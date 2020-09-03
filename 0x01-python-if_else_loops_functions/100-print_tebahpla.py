781538c56c0762a7f6c0191b681ba6efc8b6454d
BigObasheer <838@holbertonschool.com>
2020-09-03 02:40:32 +0000

    Add Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        print("{}".format(chr(i)), end="")
-    else:
-        print("{}".format(chr(i)), end="")
+for i in range(122, 97, -2):
+    print("{:c}{:c}".format((i), (i - 33)), end="")
