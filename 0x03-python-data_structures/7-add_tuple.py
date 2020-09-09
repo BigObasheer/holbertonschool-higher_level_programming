aec520aa399127ac8b33f27eca20e8418207a5c5
BigObasheer <838@holbertonschool.com>
2020-09-09 04:01:29 +0000

    Add Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_tuple(a=(), b=()):
+    if len(a) == 0:
+        a = (0, 0)
+    if len(a) == 1:
+        a= (a[0], 0)
+    if len(b) == 0:
+        b = (0, 0)
+    if len(b) == 1:
+        b = (b[0], 0)
+
+    new = (a[0] + b[0], a[1] + b[1])
+    return new
