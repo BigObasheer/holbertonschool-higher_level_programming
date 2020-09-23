2398c74a8d7b887c914555a2c91e75202d6a7431
BigObasheer <838@holbertonschool.com>
2020-09-23 03:57:19 +0000

    Add Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3y
+"""Class Square"""
+
+
+class Square:
+    """A square class"""
+    __size = None
+
+    def __init__(self, size=0):
+        """
+        Class Method
+        Args:
+            self: square
+            size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Method that returns the area
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Method that gets the current size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Method that sets the value
+        Args:
+            self: refers back to the class
+            value: value
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def my_print(self):
+        """ Method that prints out a square size * size """
+        if self.__size == 0:
+            print()
+            return
+        for height in range(self.__size):
+            for width in range(self.__size):
+                print("#", end="")
+            print()
