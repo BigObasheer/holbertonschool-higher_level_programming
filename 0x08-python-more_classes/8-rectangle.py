688ef4aa95fe78f25c7b7058e205b55d282037ae
BigObasheer <838@holbertonschool.com>
2020-09-30 22:34:07 +0000

    Add Task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -89,7 +89,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         del self
 
-     @staticmethod
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """ Returns biggest rectangle based on area """
         if not isinstance(rect_1, Rectangle):
