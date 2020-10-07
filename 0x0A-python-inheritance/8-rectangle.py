0b7b2b906185be0a39316257c118e01d02abfe0c
BigObasheer <838@holbertonschool.com>
2020-10-07 01:02:05 +0000

    Add Task 8 and 9

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Inheritance """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle"""
+    def __init__(self, width, height):
+        """ Initialize """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
