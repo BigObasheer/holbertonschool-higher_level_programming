5c0a9936e820e91dfa4ba1530ef95de6b300cc8e
BigObasheer <838@holbertonschool.com>
2020-10-07 04:05:43 +0000

    Add Task 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@
 
 def add_attribute(a, name, other):
     """ try to add a new attribute """
-    if hasattr(obj, "__dict__"):
-        setattr(obj, key, value)
+    if hasattr(a, "__dict__"):
+        setattr(a, name, value)
     else:
         raise TypeError("can't add new attribute")
