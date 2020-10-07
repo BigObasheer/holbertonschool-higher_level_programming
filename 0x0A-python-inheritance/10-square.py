67481b40389ffdc50597cce2149326514fc048ef
BigObasheer <838@holbertonschool.com>
2020-10-07 02:00:58 +0000

    Add Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """ Inheritance """
 Rectangle = __import__('9-rectangle').Rectangle
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 
 class Square(Rectangle):
     """ Class Square """
     def __init__(self, size):
         """ Initialize """
-        super().__init__(size, size)
         self.__size = size
         self.integer_validator("size", size)
+        super().__init__(size, size)
 
     def area(self):
         return self.__size * self.__size
