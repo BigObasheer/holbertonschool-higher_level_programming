cde2814473f2f54072bd933611b28c28f436b298
BigObasheer <838@holbertonschool.com>
2020-10-08 02:02:45 +0000

    Add Task 1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -3,8 +3,8 @@
 
 
 def number_of_lines(filename=""):
-""" returns the number of lines of a text file """
-with open(filename, mode="r", encoding="utf-8") as buffer:
+    """ returns the number of lines of a text file """
+    with open(filename, mode="r", encoding="utf-8") as buffer:
         i = 0
         for lines in buffer:
             i += 1
