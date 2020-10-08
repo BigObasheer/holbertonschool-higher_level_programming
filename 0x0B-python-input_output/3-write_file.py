9f48b961ad84236dcb9e1e38491c3c5ec3c832c1
BigObasheer <838@holbertonschool.com>
2020-10-08 02:22:00 +0000

    Add Task 3

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -3,7 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """ writes a string to a text file (UTF8) and returns the number of char """
+    """ writes a string to a text file and returns the number of chars """
     with open(filename, mode="w", encoding="utf-8") as buffer:
         count = buffer.write(text)
     return count
