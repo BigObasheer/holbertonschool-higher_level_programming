cf08c49cfd97a85a084457d793601f3102f8f4dc
BigObasheer <838@holbertonschool.com>
2020-10-08 03:22:57 +0000

    Add Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Input/Output """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """ Initialize"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Return a dict of self """
+        return vars(self)
