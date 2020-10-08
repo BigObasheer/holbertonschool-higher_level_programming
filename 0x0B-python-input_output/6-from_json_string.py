d51ec4e537f2676fb99bba865cc7deab2154f1f2
BigObasheer <838@holbertonschool.com>
2020-10-08 02:37:17 +0000

    Add Task 6

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Input/Output  """
+import json
+
+
+def from_json_string(my_str):
+    """ returns an object represented by the JSON string """
+    return json.dumps(my_str)
