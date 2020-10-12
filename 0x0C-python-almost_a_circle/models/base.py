47e32a6b160c9100c7b60a4859b996cd20587929
Osama <838@holbertonschool.com>
2020-10-12 15:18:18 -0500

    Add Task 0, 1, 2, and 3

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" Almost a Circle """
+
+
+class Base:
+    """ Class Base """
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """ Class Constructor """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
