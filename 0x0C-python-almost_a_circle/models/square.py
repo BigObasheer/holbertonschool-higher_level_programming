87d9a5dd3aa4b4ec738bef3426e64e6db76388e8
BigObasheer <838@holbertonschool.com>
2020-10-13 17:59:56 +0000

    Add Task 10 and 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" Class Square """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Class Square that inherits from Rectangle """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initialize """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ Returns Size Value """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Sets Size Value """
+        self.width = value
+        self.height = value
