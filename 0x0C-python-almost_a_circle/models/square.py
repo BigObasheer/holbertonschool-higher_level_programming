ef0595c0ddd3510266573c9615886784c0b73a4a
BigObasheer <838@holbertonschool.com>
2020-10-13 20:40:09 +0000

    Fix Task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,7 @@ class Square(Rectangle):
     """ Class Square that inherits from Rectangle """
     def __init__(self, size, x=0, y=0, id=None):
         """ Initialize """
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
@@ -19,3 +20,19 @@ class Square(Rectangle):
         """ Sets Size Value """
         self.width = value
         self.height = value
+
+    def __str__(self):
+        """ String Method """
+        s = "[Square] ({}) {}/{} - {}"
+        s = s.format(self.id, self.x, self.y, self.size)
+        return s
+
+    def update(self, *args, **kwargs):
+        """ Update """
+        if args:
+            attrs = ['id', 'size', 'x', 'y']
+            for index in range(len(args)):
+                setattr(self, attrs[index], args[index])
+            else:
+                for k, v in kwargs.items():
+                    setattr(self, k, v)
