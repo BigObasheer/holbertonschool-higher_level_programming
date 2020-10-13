1b556aaa33fdeedc33e43c5afd4213b03dcef19e
BigObasheer <838@holbertonschool.com>
2020-10-13 14:49:05 +0000

    Add Task 6, 7 and 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,73 +1,84 @@
 #!/usr/bin/python3
-""" Almost a Circle """
+""" Rectangle that Inherits Base """
 from models.base import Base
 
 
 class Rectangle(Base):
-    """ Class Rectangle """
-
+    """ Class Rectangle that Inherits Base """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Class Constructor """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        """ Returns Width Value"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value <= 0:
-                raise ValueError("width must be > 0")
-            print("Passed")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """ Sets Width Value """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            return self.__height
+    @property
+    def height(self):
+        """ Returns Height Value """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if value <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """ Sets Height Value """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            return self.__x
+    @property
+    def x(self):
+        """ Returns x Value """
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            if type(value) is not int:
-                raise TypeError("x must be an integer")
-            if value < 0:
-                raise ValueError("x must be >= 0")
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """ Sets x Value """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @property
-        def y(self):
-            return self.__y
+    @property
+    def y(self):
+        """ Returns y Value """
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            if type(value) is not int:
-                raise TypeError("y must be an integer")
-            if value < 0:
-                raise ValueError("y must be >= 0")
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """ Sets y Value """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
     def area(self):
         """ Area of a Rectangle """
         return self.__height * self.__width
 
     def display(self):
-        """ Display """
-        for i in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
+        for height in range(self.__height):
+            print(" " * self.__x, end="")
+            print("#" * self.__width)
+
+    def __str__(self):
+        """ String Representation of Class """
+        r = "[Rectangle] ({}) {}/{} - {}/{}"
+        r = r.format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return r
