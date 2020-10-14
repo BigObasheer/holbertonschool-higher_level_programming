1a2ba146bf71150fe6d87f6b733457a719bacacb
BigObasheer <838@holbertonschool.com>
2020-10-14 03:31:52 +0000

    Add Task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """ Almost a Circle """
 from json import dumps as to_json
 from json import dump as to_json_file
+import json
 import csv
 
 
@@ -19,12 +20,21 @@ class Base:
 
     @staticmethod
     def to_json_string(li_of_dict):
+        """ Converts to JSON """
         return to_json(li_of_dict) if type(li_of_dict) is list else '[]'
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """ Saves to File """
         buff = []
         if buff is not None:
             buff = [x.to_dictionary() for x in list_objs]
             with open(cls.__name__ + ".json", "w") as f:
                 f.write(cls.to_json_string(buff))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns JSON string representation """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
