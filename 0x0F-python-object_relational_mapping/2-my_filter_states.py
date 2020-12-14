d08c6d8984832b6c29106ca1f08332d1a431cebf
BigOBasheer <838@holbertonschool.com>
2020-12-14 05:29:07 +0000

    Add Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """ displays all values in the states table where name matches the argument """
-import sys
+from sys import argv
 import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    user, passwd, database, search = argv[1], argv[2], argv[3], argv[4]
+    db = MySQLdb.connect('localhost', user, passwd, database)
+
     find = db.cursor()
+
     find.execute("SELECT * FROM states\
                     WHERE BINARY name='{}'".format(search))
 
@@ -20,5 +17,5 @@ if __name__ == "__main__":
     for state in data:
         print(state)
 
-    find.close()
+    lookup.close()
     db.close()
