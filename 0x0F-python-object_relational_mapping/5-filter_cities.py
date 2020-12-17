fb72a07f9990639fb5380c119afbdea16caaab6a
BigOBasheer <838@holbertonschool.com>
2020-12-17 21:17:56 +0000

    Add Task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# Print all cities
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    user, passwd, database, search = argv[1], argv[2], argv[3], argv[4]
+    db = MySQLdb.connect('localhost', user, passwd, database)
+
+    find = db.cursor()
+
+    find.execute("SELECT cities.name\
+                    FROM cities JOIN states\
+                    ON cities.state_id = states.id\
+                    WHERE states.name=%s\
+                    ORDER BY states.id ASC;", (search,))
+
+    data = lookup.fetchall()
+
+    for cities in range(len(data)):
+        print(data[cities][0], end='')
+        if cities < len(data) - 1:
+            print(', ', end='')
+    print()
+
+    find.close()
+    db.close()
