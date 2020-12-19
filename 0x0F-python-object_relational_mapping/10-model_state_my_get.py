50fc23c582edd3c54ff37ccfef6a146f31746bcc
BigOBasheer <838@holbertonschool.com>
2020-12-19 05:58:36 +0000

    Add Task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """ Script that lists all State objects from the database hbtn_0e_6_usa """
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2],
-        sys.argv[3]), pool_pre_ping=True)
+    user, passwd, database = argv[1], argv[2], argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, database),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
