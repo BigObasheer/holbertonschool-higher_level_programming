000b44512160468da302357d777547d162c81969
BigOBasheer <838@holbertonschool.com>
2020-12-19 05:31:21 +0000

    Add Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# cript that lists all State objects from the database hbtn_0e_6_usa
+""" cript that lists all State objects from the database hbtn_0e_6_usa """
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
