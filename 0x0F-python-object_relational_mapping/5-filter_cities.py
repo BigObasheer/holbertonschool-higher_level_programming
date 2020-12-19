3289f2056e862883fbdcdd106caca6e29a696d96
BigOBasheer <838@holbertonschool.com>
2020-12-19 05:26:39 +0000

    Add Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     find.execute("SELECT cities.name FROM cities\
         JOIN states ON cities.state_id = states.id\
         WHERE states.name=%s\
-        ORDER BY 'cities.id' ASC;", (search,) )
+        ORDER BY 'cities.id' ASC;", (search,))
     data = find.fetchall()
     for cities in range(len(data)):
         print(data[cities][0], end='')
