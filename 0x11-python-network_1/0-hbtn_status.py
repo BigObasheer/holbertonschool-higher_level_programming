68f3250a522493e91ca351b199739836869a6f05
BigOBasheer <838@holbertonschool.com>
2021-01-12 04:56:52 +0000

    Add Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ fetches https://intranet.hbtn.io/status """
-import urllib.request
+from urllib import request
 
 
 if __name__ == "__main__":
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        with request.urlopen('https://intranet.hbtn.io/status') as response:
             web = response.read()
             print('Body response:')
             print("\t- type: {}".format(type(web)))
