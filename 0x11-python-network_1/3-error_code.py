6b48d5f17083b87438401ddf66efd23de18f3093
BigOBasheer <838@holbertonschool.com>
2021-01-12 05:07:52 +0000

    Add Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Error Code """
+from urllib import request, error
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as f:
+            print(f.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
